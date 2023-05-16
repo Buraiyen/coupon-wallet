a9d586c1378d4eb7f02fcc3592b5a0c7661d8919
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-16 14:52:34 -0700

    :lipstick: style: coupon code section

--- components/coupon-card.vue
+++ components/coupon-card.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-[600px] h-32 text-white border-2 border-solid rounded border-red-500 grid grid-cols-3"
+    class="w-[600px] h-48 text-white border-2 border-solid rounded border-red-500 grid grid-cols-3"
   >
     <div class="bg-rose-400 flex items-center justify-center col-span-1">
       <h3 class="text-7xl">30</h3>
@@ -9,7 +9,16 @@
         <p class="text-3xl">OFF</p>
       </div>
     </div>
-    <div class="bg-rose-900 col-span-2 text-center">Section 2</div>
+    <div class="bg-rose-900 col-span-2 text-center">
+      <div class="container mx-auto px-4">
+        <p class="uppercase font-bold">Your promo code:</p>
+        <p
+          class="uppercase border-solid font-bold border-2 border-rose-600 py-6 text-4xl rounded-lg"
+        >
+          Giftcode
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
