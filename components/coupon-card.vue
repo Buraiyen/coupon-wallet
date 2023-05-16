372641c4a03f04f31dfd7a13ae8bb5f9cbb375d1
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-16 14:43:34 -0700

    :lipstick: style: starting coupon code style

--- components/coupon-card.vue
+++ components/coupon-card.vue
@@ -1,5 +1,16 @@
 <template>
-  <h3 class="bg-blue-500 w-20 border-solid border-r-2">Coupon Name</h3>
+  <div
+    class="w-[600px] h-32 text-white border-2 border-solid rounded border-red-500 grid grid-cols-3"
+  >
+    <div class="bg-rose-400 flex items-center justify-center col-span-1">
+      <h3 class="text-7xl">30</h3>
+      <div class="grid grid-rows-2">
+        <p class="text-5xl">%</p>
+        <p class="text-3xl">OFF</p>
+      </div>
+    </div>
+    <div class="bg-rose-900 col-span-2 text-center">Section 2</div>
+  </div>
 </template>
 
 <script>
