e1878969787c6a28fd961756e9f2d54b32429b2b
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-19 13:14:24 -0700

    :sparkles: feat: init coupon-card-list

---
+++ components/coupon-card-list.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+    <coupon-card />
+    <coupon-card />
+    <coupon-card />
+    <coupon-card />
+    <coupon-card />
+  </section>
+</template>
+
+<script>
+import couponCard from './coupon-card.vue';
+export default {
+  components: { couponCard },
+};
+</script>
