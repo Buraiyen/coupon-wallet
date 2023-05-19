709abb89e384ae2defef28d91c3305b3a8b16ea1
Brian Nguyen <brian.edison.nguyen@gmail.com>
2023-05-19 13:05:27 -0700

    :lipstick: style: responsive button

--- components/coupon-card.vue
+++ components/coupon-card.vue
@@ -21,7 +21,7 @@
         <!-- Gift code section -->
         <div>
           <button
-            class="text-rose-800 bg-gray-100 rounded-md border-rose-300 border-solid border-2 py-2 px-6 transition-colors hover:bg-rose-200 disabled:bg-gray-400 disabled:text-gray-600 disabled:border-gray-500"
+            class="text-rose-800 bg-gray-100 rounded-md border-rose-300 border-solid border-2 py-2 w-[33%] transition-colors hover:bg-rose-200 disabled:bg-gray-400 disabled:text-gray-600 disabled:border-gray-500"
             @click="redeem"
             :disabled="isRedeemed"
           >
